e0105f2ff8aca38725f775da2bf02334da669afd
Techroy23 <53241974+techroy23@users.noreply.github.com>
2021-10-14 10:26:40 +0800

    Update MAIN.bat

--- MAIN.bat
+++ MAIN.bat
@@ -1,34 +1,69 @@
 REM LATENCY CHECK BY TECHROY23
 
-REM << Change me >>
-SET IP=1.1.1.1
-REM << Change me >>
-
 @echo off
-
-mode 17,7
-
+mode 30,20
 cls
+echo.
+echo.
+echo.
+echo.
+
+echo   PLEASE
+echo   WAIT
+echo   WHILE
+echo   WE
+echo   WARM
+echo   UP
+echo   .....
+
+set IP1=1.1.1.1
+set IP2=1.0.0.1
+set IP3=8.8.8.8
+set IP4=8.8.4.4
 
 :title
-TITLE -PING-
+TITLE Latency Checker
 
 :main
-CALL:latency %ip%
+CALL:latency %IP1% %IP2% %IP3% %IP4%
 
 :screen
 cls
 ECHO.
-echo  PING %ip%
-echo  TEST-1 %q%
-echo  TEST-2 %qq%
-echo  TEST-3 %qqq%
-echo  TEST-4 %qqqq%
+echo  DNS      TEST-1
+echo  %IP1% %T1A%
+echo  %IP2% %T1B%
+echo  %IP3% %T1C%
+echo  %IP4% %T1D%
+
+ECHO.
+echo  DNS      TEST-2
+echo  %IP1% %T2A%
+echo  %IP2% %T2B%
+echo  %IP3% %T2C%
+echo  %IP4% %T2D%
+
+ECHO.
+echo  DNS      TEST-3
+echo  %IP1% %T3A%
+echo  %IP2% %T3B%
+echo  %IP3% %T3C%
+echo  %IP4% %T3D%
 
 GOTO title
 
 :latency
-FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET q=%%i
-FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET qq=%%i
-FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET qqq=%%i
-FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET qqqq=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET T1A=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %2 ^| FIND "ms"') DO SET T1B=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %3 ^| FIND "ms"') DO SET T1C=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %4 ^| FIND "ms"') DO SET T1D=%%i
+
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET T2A=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %2 ^| FIND "ms"') DO SET T2B=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %3 ^| FIND "ms"') DO SET T2C=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %4 ^| FIND "ms"') DO SET T2D=%%i
+
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %1 ^| FIND "ms"') DO SET T3A=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %2 ^| FIND "ms"') DO SET T3B=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %3 ^| FIND "ms"') DO SET T3C=%%i
+FOR /F "tokens=4 delims== skip=1" %%i IN ('ping.exe -n 1 -l 1 %4 ^| FIND "ms"') DO SET T3D=%%i
